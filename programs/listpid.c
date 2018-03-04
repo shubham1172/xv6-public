b415d2b190011bb113d525911ed84eec42ca86b5
Anuja <anujanegi1@mail.com>
2018-03-04 10:18:53 +0530

    Updated tabs in listpid user program

--- programs/listpid.c
+++ programs/listpid.c
@@ -8,11 +8,11 @@ main()
 {
   int i;
   struct pstat p;
-  printf(1, "Sno \t PID \t High \t Low\n");
+  printf(1, "Sno\tPID\tHigh\tLow\n");
   listpid(&p);
   for( i = 0; i < NPROC ; i++) {
     if(p.inuse[i])
-      printf(1, " %d\t\t%d\t\t%d\t\t%d\n", i, p.pid[i], p.hticks[i], p.lticks[i]);
+      printf(1, "%d\t%d\t%d\t%d\n", i, p.pid[i], p.hticks[i], p.lticks[i]);
   }
   exit();
 }
