91c91ee01ba241308b69d56f9e23132163ef9223
Shubham Sharma <shubhamsharma1172@gmail.com>
2018-03-13 18:14:10 +0530

    Modified mv

--- programs/mv.c
+++ programs/mv.c
@@ -1,13 +1,17 @@
 #include "types.h"
-#include "stat.h"
 #include "user.h"
+#include "copy.h"
 
 int main (int argc, char *argv[]){
   if (argc != 3){
     printf(2, "Usage: mv source destination\n");
     exit();
   }
-  if ((link(argv[1], argv[2]) < 0) || (unlink(argv[1]) < 0))
+  if(copy(argv[1], argv[2]) < 0){
+    printf(2, "mv: cannot move %s to %s\n", argv[1], argv[2]);
+    exit();
+  }
+  if (unlink(argv[1]) < 0)
     printf(2, "mv: move %s to %s failed\n", argv[1], argv[2]);
   exit();
 }
